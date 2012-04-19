6bca9702fe480608ee49d5880333f95478a4da73
Eric Dostie <dostieer@msu.edu>
2012-04-19 20:51:41 +0000

    Bash script to run django using squawker

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 squawker
 ========
 
-A copy of twitter (squeak squawk)
+A copy of Twiter (sort of) (squeak squawk)
+
+Run via 'python manage.py runserver 0.0.0.0:80' or 'run-squawker.sh inside root of git directory' (with user execute bit ON)
---
+++ run-squawker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Runs squawker through django's webserver on port 80
+# Make sure the user has executable bit on this file (chmod u+x run-squawker.sh if not)
+# Accessible via EC2 URL or localhost:80 (if running, obviously)
+python manage.py runserver 0.0.0.0:80
