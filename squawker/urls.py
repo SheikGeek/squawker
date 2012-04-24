8af864220230bd841a8dc482e5888e0c685929dd
Chelsea Carr <carrche2@msu.edu>
2012-04-24 13:06:35 -0400

    adding setting files

--- squawker/urls.py
+++ squawker/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from squawker import settings
+
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,6 +12,10 @@ urlpatterns = patterns('',
     url(r'^$', 'squawker.views.index'),
     url(r'^logout/$', 'squawker.views.logout'),
     url(r'^create_user/$', 'squawker.views.create_user'),
+    url(r'^mymedia/(?P<path>.*)$', 'django.views.static.serve',  
+         {'document_root':     settings.MEDIA_ROOT}),
+    #url(r'^css/(?*.css)$', 'django.views.static.serve', {'document_root': '/css/'}),
+
     # url(r'^squawker/', include('squawker.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
