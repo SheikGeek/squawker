ffe6c47a5f21a4504122a01010b3e7628a2891a2
Eric Dostie <dostieer@msu.edu>
2012-04-24 13:47:14 -0400

    Listing messages implemented

--- messages/views.py
+++ messages/views.py
@@ -10,21 +10,30 @@ from messages.models import Message
 
 # list messages
 def list_messages(request):
-    # TODO implement
-    pass
+	message_objects = Message.objects.all()
+	messages = []
+	for x in message_objects:
+		user = User.objects.get(id=x.user_id)
+		new_tuple = (x.message, user.username)
+		messages.append(new_tuple)
+	messages.reverse()
+	# create the context for the template
+	context = {}
+	context['messages'] = messages
+	return render_to_response("list_messages.html", context)
 
 # create a new message
 def create_message(request):
-    # only logged in users can write messages
-    if request.user.is_authenticated():
-        user_id = request.session['_auth_user_id']
-        if request.method == "GET":
-            context = {}
-            context.update(csrf(request))
-            return render_to_response("create_message.html", context)
-        else:
-            content = request.POST['message']
-            # create the message
-            message = Message(message=content, user_id=user_id)
-            message.save()
-    return redirect('/')
+	# only logged in users can write messages
+	if request.user.is_authenticated():
+		user_id = request.session['_auth_user_id']
+		if request.method == "GET":
+			context = {}
+			context.update(csrf(request))
+			return render_to_response("create_message.html", context)
+		else:
+			content = request.POST['message']
+			# create the message
+			message = Message(message=content, user_id=user_id)
+			message.save()
+	return redirect('/')
