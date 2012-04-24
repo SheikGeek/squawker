1277fee0fba913f9f082681a751aeaa87e21a963
Chelsea Carr <carrche2@msu.edu>
2012-04-24 13:47:11 -0400

    tried something else on css

--- squawker/settings.py
+++ squawker/settings.py
@@ -1,4 +1,5 @@
 # Django settings for squawker project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -48,7 +49,8 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '../media/'  #absolute path to media
+root = os.path.dirname(__file__)
+MEDIA_ROOT = root[:-8]+'media/'  #absolute path to media
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
