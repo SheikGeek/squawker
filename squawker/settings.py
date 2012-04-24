db75c77ad3011d1b3a8fec9461a02875baec4e8e
Eric Dostie <dostieer@msu.edu>
2012-04-24 14:17:52 -0400

    static file settings, removing media settings

--- squawker/settings.py
+++ squawker/settings.py
@@ -1,5 +1,4 @@
 # Django settings for squawker project.
-import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -49,19 +48,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-root = os.path.dirname(__file__)
-MEDIA_ROOT = root[:-8]+'media/'  #absolute path to media
+MEDIA_ROOT = 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL ='/mymedia/' #because admin already using /media
+MEDIA_URL =
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = './static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
